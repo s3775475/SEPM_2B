e28c6fe434289d49f0cc5bf4762535073d133b0f
chris <s3775475@student.rmit.edu.au>
2022-03-17 22:21:02 +1100

    Initial commit

---
+++ src/main/java/com/sepm_2b/packages/Sepm2BApplication.java
@@ -0,0 +1,13 @@
+package com.sepm_2b.packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sepm2BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sepm2BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sepm_2b/packages/Sepm2BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepm_2b.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sepm2BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
